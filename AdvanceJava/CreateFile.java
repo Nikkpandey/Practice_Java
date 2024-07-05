690624f65c976ef061a13d9e45b0296dea8adc12
Nikkpandey <nikhilpandey2023@gmail.com>
2024-07-05 11:20:07 +0530

    Java File Practice

--- AdvanceJava/CreateFile.java
+++ AdvanceJava/CreateFile.java
@@ -2,9 +2,16 @@ package AdvanceJava;
 import java.io.*;
 public class CreateFile {
     public static void main(String args[]) throws IOException{
+
+        //Attach keyboard to input stream
         DataInputStream dis=new DataInputStream(System.in);
+
+        //Attach file where data stored
         FileOutputStream fos=new FileOutputStream("myfile.txt",true);
+
+        // for improving performance
         BufferedOutputStream bos=new BufferedOutputStream(fos,1024);
+
         System.out.println("Enter the text (@ at the end): ");
         char ch;
 
